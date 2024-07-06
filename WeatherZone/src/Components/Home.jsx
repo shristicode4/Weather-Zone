3bde85794ccaddbe024b478c6c8078c675bce5c9
shristi <shristikaadhikari88@gmail.com>
2024-07-06 23:04:15 +0530

    Home

--- WeatherZone/src/Components/Home.jsx
+++ WeatherZone/src/Components/Home.jsx
@@ -4,58 +4,71 @@ import { Link } from "react-router-dom";
 import HomeIcon from "@mui/icons-material/Home";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import { Typography, Box } from "@mui/material";
-import backgroundImage from "../assets/Homebackground.jpg";
+
+import "../Styles/Home.scss";
 
 const Home = () => {
   return (
     <>
-      <Button
-        variant="text"
-        color="success"
-        size="medium"
-        component={Link}
-        to="/Home"
-        sx={{ ml: 130, mr: 0 }}
-        startIcon={<HomeIcon />}
+      <section
+        className="Homeroot"
+        style={{ backgroundColor: "black", height: "100vh" }}
       >
-        Home
-      </Button>
-      ~
-      <Button
-        variant="text"
-        color="success"
-        size="medium"
-        component={Link}
-        to="/Dashboard"
-        sx={{ ml: 5, mr: 0 }}
-        startIcon={<DashboardIcon />}
-      >
-        Dashboard
-      </Button>
-      <Button
-        variant="contained"
-        color="secondary"
-        size="medium"
-        component={Link}
-        to="/Login"
-        sx={{ ml: 15 }}
-      >
-        Login
-      </Button>
-      <div>
-        <Typography
-          variant="h2"
-          color="secondary"
-          align="center"
-          sx={{ mt: 30, fontWeight: "bold" }}
-        >
-          Welcome to WeatherZone
-        </Typography>
+        <div className="content">
+          <Button
+            variant="contained"
+            color="success"
+            size="medium"
+            component={Link}
+            to="/Home"
+            sx={{ ml: 130, mr: 0, mt: 2 }}
+            startIcon={<HomeIcon />}
+          >
+            Home
+          </Button>
 
-        <Typography variant="h6" color="Primary" align="center" sx={{ md: 4 }}>
-          Let's dive deep into the weather conditions of your choosen cities
-        </Typography>
-      </div>
+          <Button
+            variant="contained"
+            color="success"
+            size="medium"
+            component={Link}
+            to="/Dashboard"
+            sx={{ ml: 5, mr: 0, mt: 2 }}
+            startIcon={<DashboardIcon />}
+          >
+            Dashboard
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            size="medium"
+            component={Link}
+            to="/Login"
+            sx={{ ml: 10, mt: 2, padding: "5px 30px" }}
+          >
+            Login
+          </Button>
+          <div>
+            <Typography
+              variant="h2"
+              color="secondary"
+              align="center"
+              sx={{ mt: 30, fontWeight: "bold" }}
+            >
+              Welcome to WeatherZone
+            </Typography>
+
+            <Typography
+              variant="h6"
+              color="black"
+              align="center"
+              sx={{ md: 4 }}
+            >
+              Let's dive deep into the weather conditions of your choosen cities
+            </Typography>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
