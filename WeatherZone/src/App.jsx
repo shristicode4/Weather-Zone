4e33c9ef07c2f806c2c7fefad4499ab3143b93ec
shristi <shristikaadhikari88@gmail.com>
2024-07-07 22:16:55 +0530

    login

--- WeatherZone/src/App.jsx
+++ WeatherZone/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Components/Home";
 import Dashboard from "./Components/Dashboard";
 import Login from "./Components/Login";
+import SignUp from "./Components/SignUp";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/Home" element={<Home />} />
         <Route path="/Dashboard" element={<Dashboard />} />
         <Route path="/Login" element={<Login />} />
+        <Route path="/signup" component={<SignUpPage />} />
       </Routes>
     </Router>
   );
