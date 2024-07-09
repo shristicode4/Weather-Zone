afbccd5660fecab9c2f38bb157957e4d3002eeeb
Shristicode4 <shristikaadhikari88@gmail.com>
2024-07-09 23:56:50 +0530

    search bar

--- WeatherZone/src/Components/Dashboard.jsx
+++ WeatherZone/src/Components/Dashboard.jsx
@@ -1,12 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
+import { styled, alpha } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
 import Toolbar from "@mui/material/Toolbar";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import WbSunnyIcon from "@mui/icons-material/WbSunny";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
 
 const Dashboard = () => {
   return (
@@ -25,6 +53,16 @@ const Dashboard = () => {
             sx={{ mr: 2 }}
           >
             <WbSunnyIcon sx={{ color: "white" }} />
+
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ "aria-label": "search" }}
+              />
+            </Search>
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Welcome to weather zone
