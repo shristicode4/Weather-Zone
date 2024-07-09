4f71dc35827cba7e351b09446ab6e9795b41e0f5
Shristicode4 <shristikaadhikari88@gmail.com>
2024-07-09 23:58:07 +0530

    edit

--- WeatherZone/src/Components/Dashboard.jsx
+++ WeatherZone/src/Components/Dashboard.jsx
@@ -10,32 +10,6 @@ import IconButton from "@mui/material/IconButton";
 import WbSunnyIcon from "@mui/icons-material/WbSunny";
 import SearchIcon from "@mui/icons-material/Search";
 
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(3),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
 const Dashboard = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -53,16 +27,6 @@ const Dashboard = () => {
             sx={{ mr: 2 }}
           >
             <WbSunnyIcon sx={{ color: "white" }} />
-
-            <Search>
-              <SearchIconWrapper>
-                <SearchIcon />
-              </SearchIconWrapper>
-              <StyledInputBase
-                placeholder="Search…"
-                inputProps={{ "aria-label": "search" }}
-              />
-            </Search>
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Welcome to weather zone
